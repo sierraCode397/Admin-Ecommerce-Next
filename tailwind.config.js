e4f431a9d061f7ce1e802b6c484e157797fe61c2
izaack107 <izaack107@gmail.com>
2023-03-17 23:05:07 -0600

    Añadiendo estilos con tailwind y postcss

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        lightBlue: colors.sky,
+        warmGray: colors.stone,
+        trueGray: colors.gray,
+        coolGray: colors.coolGray,
+        blueGray: colors.slate,
+      }
+    },
+  },
+  plugins: [],
+};
