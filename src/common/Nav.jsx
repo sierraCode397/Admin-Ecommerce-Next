ec195bd17b2968373469a1d2242952bc32dc85dd
izaack107 <izaack107@gmail.com>
2023-03-18 14:44:09 -0600

    Implementacion de la ruta login

--- src/common/Nav.jsx
+++ src/common/Nav.jsx
@@ -3,11 +3,12 @@ import { useRouter } from 'next/router';
 export default function Nav() {
   const router = useRouter();
   const route = router.pathname.substring(1);
+  const capitalizedRoute = route.charAt(0).toUpperCase() + route.slice(1).toLowerCase();
 
   return (
     <nav className="bg-white shadow">
       <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <h1 className="text-3xl font-bold text-gray-900 capitalize">{route}</h1>
+        <h1 className="text-3xl font-bold text-gray-900 capitalize my-4">{capitalizedRoute}</h1>
       </div>
     </nav>
   );
